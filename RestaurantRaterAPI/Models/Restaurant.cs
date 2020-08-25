04dfe037fef7ad4ed2053fa9639c8fb77a25b7a5
Dgraber27 <drewg2727@gmail.com>
2020-08-25 10:40:12 -0400

    Added Restaurant entity

--- RestaurantRaterAPI/Models/Restaurant.cs
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace RestaurantRaterAPI.Models
 {
+    //Restaurant entity (Class that gets stored in the database)
     public class Restaurant
     {
+        [Key] //attribute
+        public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public double Rating { get; set; }
         public bool IsRecommended // => Rating >= 3.5;   simplified version since only one operation
         {
