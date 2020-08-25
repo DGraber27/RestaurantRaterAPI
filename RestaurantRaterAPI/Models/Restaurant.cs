d75bafd579881949b28dd338fd172a878687fb57
Dgraber27 <drewg2727@gmail.com>
2020-08-25 10:38:57 -0400

    Added Restaurant entity

---
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models
+{
+    public class Restaurant
+    {
+        public string Name { get; set; }
+        public double Rating { get; set; }
+        public bool IsRecommended // => Rating >= 3.5;   simplified version since only one operation
+        {
+            get
+            {
+                return Rating >= 3.5;
+                //return (Rating >= 3.5) ? true : false;   Could also be if/else statement
+            }
+        }
+
+
+    }
+}
